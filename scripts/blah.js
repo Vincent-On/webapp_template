454b5513e5f4f3b245fe59e51c87dbaf8400b34c
Vincent On <vincentonproduction@gmail.com>
2021-03-23 15:05:24 -0700

    new stuff

--- scripts/blah.js
+++ scripts/blah.js
@@ -1,1 +1,17 @@
 
+function sayHello(){
+    firebase.auth().onAuthStateChanged(function(somebody){
+        if(somebody){
+            console.log(somebody.uid);
+            db.collection("users").doc(somebody.uid)
+            .get()
+            .then(function(doc){
+                console.log(doc.data().name);
+                var n = doc.data().name;
+                $("#username").text(n);
+            })
+
+        }
+    })
+}
+sayHello();
