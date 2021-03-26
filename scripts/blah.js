bf0b466caca3ea90854e5ef14c21e61f37a2882a
Ashkan-Rahnavardi <ashkan.rahnavardi@gmail.com>
2021-03-25 21:48:06 -0700

    Added Restaurant Catagories

--- scripts/blah.js
+++ scripts/blah.js
@@ -1,30 +1,19 @@
-
-function sayHello(){
-    firebase.auth().onAuthStateChanged(function(somebody){
-        if(somebody){
-            console.log(somebody.uid);
-            db.collection("users").doc(somebody.uid)
-            .get()
-            .then(function(doc){
-                console.log(doc.data().name);
-                var n = doc.data().name;
-                $("#username").text(n);
-            })
-
-        }
-    })
-}
-sayHello();
-
-
 function writeCategories() {
-    var categoriesRef = db.collection("categories");
+    var categoriesRef = db.collection("Restaurant Categories");
+    
     categoriesRef.add({
-        name: "Samurai Sushi",
-		Japanese: true,             //
-        Asian: true,
-        American: false,
-        BBQ: false,
+        code: "FAST",
+        name: "Fast Food", 
+    });
+
+    categoriesRef.add({
+        code: "SUSH",
+        name: "SUSHI", 
+    });
+
+    categoriesRef.add({
+        code: "PIZZ",
+        name: "Pizza", 
     });
 }
 
