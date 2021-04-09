a9c23848b27e9d398d708621dfba57ced6e7c547
Ashkan-Rahnavardi <ashkan.rahnavardi@gmail.com>
2021-04-08 20:11:55 -0700

    same as before

--- scripts/blah.js
+++ scripts/blah.js
@@ -60,3 +60,34 @@ function sayHello() {
     });
 }
 sayHello();
+
+
+function getDetails() {
+    // https://some.site/?id=123
+    const parsedUrl = new URL(window.location.href);
+    console.log(parsedUrl.searchParams.get("id")); // "123"
+
+    // extract id from url, assign to variable
+    var id = parsedUrl.searchParams.get("id");
+    console.log(id);
+
+    // use this ID to read from firestore
+    db.collection("Restaurants")
+        .doc(id) //webcam ID that we extracted
+        .get()
+        .then(function (doc) { // display details!
+            var name = doc.data().fields.name;
+            //var geo_area = doc.data().fields.geo_local_area;
+            //var coord = doc.data().fields.geom.coordinates;
+            //var url = doc.data().fields.url;
+            $("#details-go-here").append("<h1> " + name + "</h1>");
+        /*  $("#details-go-here").append("<h1> " + geo_area + "</h1>");
+            $("#details-go-here").append("<h1> " + coord + "</h1>");
+            $("#details-go-here").append("<a href='" + url + "' > " + url);
+            var likeid = "like" + id;
+            //$("#details-go-here").append("<h1 id= '" + likeid + "'> CLICK HERE TO LIKE </h1>");
+            $("#details-go-here").append(" <br>  <i id= '" + likeid + "' class='fas fa-thumbs-up'></i>");
+            addLikeListener(id, likeid);     */
+        }) 
+}
+getDetails();
