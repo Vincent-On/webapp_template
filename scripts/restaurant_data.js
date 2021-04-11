f00e1e0cbfa4a0c3d0ea6d063833f42d66e0f653
Vincent On <vincentonproduction@gmail.com>
2021-04-10 17:27:09 -0700

    updated

---
+++ scripts/restaurant_data.js
@@ -0,0 +1,43 @@
+let restaurant_data = {
+    "1": {
+        "Name": "A&W",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$"
+    },
+    "2": {
+        "Name": "Burger King",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$"
+    },
+    "3": {
+        "Name": "McDonalds",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$"
+    },
+    "4": {
+        "Name": "Wendy's",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$"
+    },
+    "5": {
+        "Name": "Dominos",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$$"
+    },
+    "6": {
+        "Name": "Pizza Hut",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$$"
+
+    },
+    "7": {
+        "Name": "Sushi Man",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$$$"
+    },
+    "8": {
+        "Name": "SushiTown",
+        "hours": "Hours of Operation Monday 9-9 Tuesday 9-9 Wednesday 9-9 Thursday 9-9 Friday 9-9 Saturday 9-9 Sunday 9-9",
+        "pricing": "$$$"
+    },
+};
